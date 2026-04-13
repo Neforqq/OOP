61edda236ffb2e62228b8a399f54035cee99d1ae
Neforqq <makslupaschko@gmail.com>
2026-04-13 11:09:19 +0300

    Update Main.java

--- src/university/Main.java
+++ src/university/Main.java
@@ -1,5 +1,4 @@
 package university;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -7,14 +6,21 @@ public class Main {
         Student s1 = new Student("Ivan Ivanov", 1, "IT", "TO-21", "денна");
         Student s2 = new Student("Petro Petrov", 2, "IT", "TO-22", "денна");
 
+        Teacher t1 = new Teacher("Dr. Smith", "IT", 1, 10, "123");
+
         EducationalInstitution uni =
                 new EducationalInstitution("University", "state", 1000, 100, "Odessa");
 
         uni.addStudent(s1);
         uni.addStudent(s2);
+        uni.addTeacher(t1);
 
+        System.out.println("Students:");
         uni.showStudents();
 
-        System.out.println(uni);
+        System.out.println("\nTeachers:");
+        uni.showTeachers();
+
+        System.out.println("\n" + uni);
     }
 }
