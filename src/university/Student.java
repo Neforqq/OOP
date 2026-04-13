3d80970d2d7d058e8610bb7c6135f6424f309c2d
Neforqq <makslupaschko@gmail.com>
2026-04-13 17:50:46 +0300

    Update Student.java

--- src/university/Student.java
+++ src/university/Student.java
@@ -1,4 +1,5 @@
 package university;
+
 public class Student {
 
     private String fullName;
@@ -7,16 +8,30 @@ public class Student {
     private String group;
     private String studyForm;
 
+    private int grade;
+    private int attendance;
+
     public Student(String fullName, int studentId, String faculty, String group, String studyForm) {
         this.fullName = fullName;
         this.studentId = studentId;
         this.faculty = faculty;
         this.group = group;
         this.studyForm = studyForm;
+
+        this.grade = 0;
+        this.attendance = 0;
     }
 
-    public String getGroup() {
-        return group;
+    public void addGrade(int value) {
+        this.grade += value;
+    }
+
+    public void addAttendance(int value) {
+        this.attendance += value;
+    }
+
+    public boolean isPassing() {
+        return grade >= 60 && attendance >= 70;
     }
 
     public void study() {
@@ -25,6 +40,9 @@ public class Student {
 
     @Override
     public String toString() {
-        return "Student: " + fullName + ", group=" + group;
+        return "Student: " + fullName +
+                ", group=" + group +
+                ", grade=" + grade +
+                ", attendance=" + attendance;
     }
 }
