cf78d4dbaeeb96b6d33ecab1810c493e9d0b1de5
Neforqq <makslupaschko@gmail.com>
2026-04-13 17:59:42 +0300

    Update Main.java

--- src/university/Main.java
+++ src/university/Main.java
@@ -1,4 +1,5 @@
 package university;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,11 +16,21 @@ public class Main {
         uni.addStudent(s2);
         uni.addTeacher(t1);
 
+        s1.addGrade(75);
+        s1.addAttendance(80);
+
+        s2.addGrade(55);
+        s2.addAttendance(60);
+
         System.out.println("Students:");
         uni.showStudents();
 
         System.out.println("\nTeachers:");
         uni.showTeachers();
+
+        System.out.println("\nStudent checks:");
+        System.out.println(s1 + " | passing = " + s1.isPassing());
+        System.out.println(s2 + " | passing = " + s2.isPassing());
 
         System.out.println("\n" + uni);
     }
